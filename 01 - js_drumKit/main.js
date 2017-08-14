a8de1d1412db701445fa3ffeed163dcf3127ad70
apellanr <ryan.apellanes@gmail.com>
2017-08-14 13:26:03 -0700

    fixed playing and remove css issues

--- 01 - js_drumKit/main.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('keydown', function(event){
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    if(!audio) return; // stop the function from running
-    audio.currentTime = 0; // rewinds to the start
-    audio.play(); // if you double click btn it wont play bc audio is playing already
-});
