9ec30aea9cab6b37547e75024d59421a08f9d6b8
apellanr <ryan.apellanes@gmail.com>
2017-08-21 22:22:52 -0700

    seconds rotating

--- 02 - clock/main.js
+++ 02 - clock/main.js
@@ -1,7 +1,12 @@
+const secondHand = document.querySelector('.second-hand');
+
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90; // add 90deg to offset default 90deg
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`; // ES6 template literals
     console.log(seconds);
+    
 }
 
 setInterval(setDate, 1000);
