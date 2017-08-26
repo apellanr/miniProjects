da4ce0eac122e761615d47a5f92857501b347f3a
apellanr <ryan.apellanes@gmail.com>
2017-08-25 20:25:10 -0700

    switch player functions and hideDice functions

--- dice_game/app.js
+++ dice_game/app.js
@@ -15,7 +15,7 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-document.querySelector('.dice').style.display = 'none'; // hides the dice on game load
+hideDice(); // hides the dice on game load
 
 //sets scores to 0 initially
 document.getElementById('score-0').textContent = '0';
@@ -33,17 +33,52 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.style.display = 'block';
     diceDOM.src = 'imgs/dice-' + diceNum + '.png';
     
-    // update round score ONLY IF the rolled number was not 1
-    if(diceNum !== 1) {
-        // add score
-        roundScore += diceNum;
+    if(diceNum !== 1) { // update round score ONLY IF the rolled number was not 1
+        roundScore += diceNum; // add score
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        // change players
+        switchPlayer(); // change players
     }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore; 
+    
+    // update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    // check if player has won the game
+    if(scores[activePlayer] >= 40) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        hideDice();
+    } else {
+        switchPlayer();
+    }
+});
+
+function switchPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0; // roll 1, opponent becomes active player; round score to 0
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // toggle active class when switching
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //hide dice again when player rolls a 1
+    hideDice();
+}
+
+function hideDice() {
+    document.querySelector('.dice').style.display = 'none';    
+}
 
 
-// document.querySelector('#current-' + activePlayer).textContent = diceNum;
+//document.querySelector('.player-0-panel').classList.remove('active');
+//document.querySelector('.player-1-panel').classList.toggle('active');
 // var x = document.querySelector('#score-0').textContent;
 // console.log(x);
